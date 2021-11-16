1cc6bf4a7b6689a088bbabc5a370552cda4a100e
Carolina Ambrósio <74427366+carolinAmbrosio@users.noreply.github.com>
2021-11-15 21:13:57 -0400

    Update q1Binario.c

--- q1Binario.c
+++ q1Binario.c
@@ -31,9 +31,9 @@ void selecao(int *vet, int tam){
     }
 }
  int PesquisaBinaria(int *vet, int tam, int n){
-     int comeco = 0; //Limite inferior (em C o índice inicial é zero)
+     int comeco = 0; //Limite inferior (em C o Ã­ndice inicial Ã© zero)
      int final = tam-1; //Limite superior (tamanho do vetor -1
-                            // porque o índice inicial é zero )
+                            // porque o Ã­ndice inicial Ã© zero )
      int meio;
      while (comeco <= final){
           //meio = comeco + (final-comeco)/2;
@@ -45,7 +45,7 @@ void selecao(int *vet, int tam){
           else
                comeco = meio+1;
      }
-     return -1;   // não encontrado
+     return -1;   // nÃ£o encontrado
 }
 int main(){
     clock_t t;
@@ -53,15 +53,15 @@ int main(){
 
     printf("Tamanho do vetor: ");
     scanf("%d", &tam);
-    vet = (int *) malloc(tam * sizeof(int)); // aloca a mémoria
+    vet = (int *) malloc(tam * sizeof(int)); // aloca a mÃ©moria
     preenche(vet, tam);
     selecao(vet, tam);
     imprime (vet, tam);
 
-    printf("\nValor a ser pesquisado?:");
+    clock_t begin = clock();
+    printf("\n\nValor a ser pesquisado?:");
     scanf ("%d", &n);
 
-    clock_t begin = clock();
     pos = PesquisaBinaria(vet,tam, n);
     clock_t end = clock();
     if (pos==-1){
@@ -70,7 +70,7 @@ int main(){
         printf("\n\nValor encontrado na posicao %d", pos+1);
     }
 
-    printf("\nTempo de execucao: %f segundos", (double)(end - begin) / CLOCKS_PER_SEC);
+    printf("\n\nTempo de execucao em ms: %f", (double)(end - begin) *1000 / CLOCKS_PER_SEC);
     free(vet);
 
     return 0;
